4f091d5b1aebb995dc800f6900b52fa8341a1733
Min Cho <minsangcho19@gmail.com>
2022-02-23 10:56:06 -0800

    done

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -18,9 +18,33 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  #UPDATE /plants/:id
+  def update
+    plant = Plant.find_by(id: params[:id])
+    if plant
+      plant.update(plant_params)
+      render json: plant
+    else
+      render json: {error: "Plant not found"}, status: :not_found
+    end
+  end
+
+  #DELETE /PLANTS/:ID
+
+  def destroy 
+    plant = Plant.find_by(id: params[:id])
+    if plant
+      plant.destroy
+      head :no_content
+    else
+      render json: {error: "Plant not found"}, status: :not_found
+    end
+  end 
+
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
   end
+
 end
